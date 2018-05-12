2dbffc05a812ebcd0cf06b21667c15fd1fd6242c
Andrew Minnich <aminnich3@gmail.com>
2018-05-12 14:02:22 -0400

    fix right knob display

--- source/Knob.cpp
+++ source/Knob.cpp
@@ -2,7 +2,7 @@
 
 bool Knob::Draw(IGraphics * pGraphics)
 {
-	int i = (int)(mValue * (mBitmap.N - 1));
+	int i = mValue * mBitmap.N;
 	i = BOUNDED(i, 1, mBitmap.N);
 	return pGraphics->DrawBitmap(&mBitmap, &mRECT, i, &mBlend);
 }
