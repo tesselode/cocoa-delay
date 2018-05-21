a82fcbf83cb0071c035d894c2d2f9005ef23b7a1
Andrew Minnich <aminnich3@gmail.com>
2018-05-20 22:41:22 -0400

    use airwindows spiral distortion

--- source/StatefulDrive.cpp
+++ source/StatefulDrive.cpp
@@ -2,7 +2,7 @@
 
 double StatefulDrive::Process(double input)
 {
-	auto driven = sin(input);
+	auto driven = input == 0.0 ? 0.0 : sin(input * abs(input)) / abs(input);
 	auto mix = fabs(previous + driven) * .5;
 	previous = driven;
 	return input * (1.0 - mix) + driven * mix;
